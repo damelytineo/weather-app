318ff23312a96f89eafe58fc9534aef16121ec11
Mely07 <damely.tineo07@gmail.com>
2025-10-20 11:42:12 -0400

    implement SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import iconSearch from '../assets/images/icon-search.svg';
 
 function SearchBar() {
   return (
-    <div>
-      {/* Search bar implementation goes here */}
+    <div className="search-container">
+      <input type="text" placeholder="Search for a place..." className="search-input" />
+      <img src={iconSearch} alt="Search Icon" className="search-icon" />
+      <button className="btn btn-primary search-button" type="button">Search</button>
     </div>
   );
 }
